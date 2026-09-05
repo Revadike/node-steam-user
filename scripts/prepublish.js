32f17c54dd2074b68da065602bfa65d6f8ceb8b1
Revadike <4411977+Revadike@users.noreply.github.com>
2026-09-05 18:22:53 +0200

    More lint fixes

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,7 +1,7 @@
 // This file is run by npm prior to the package being published to the registry
 
 // Update the CM list
-download('https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?format=json&cellid=0', function(data) {
+download('https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?format=json&cellid=0', function (data) {
 	let json = JSON.parse(data);
 
 	if (!json.response || json.response.result != 1) {
@@ -27,13 +27,13 @@ function download(url, callback) {
 	reqData.method = 'GET';
 
 	// This will crash if there's an error. But that's fine.
-	require('https').request(reqData, function(res) {
+	require('https').request(reqData, function (res) {
 		let data = '';
-		res.on('data', function(chunk) {
+		res.on('data', function (chunk) {
 			data += chunk;
 		});
 
-		res.on('end', function() {
+		res.on('end', function () {
 			callback(data);
 		});
 	}).end();
